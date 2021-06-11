3e5cebbcab16c01c8e547ea78c64c200fd088fcb
DeveloperRooney <han_0427@naver.com>
2021-06-11 13:17:42 +0900

    index 페이지 레이아웃 수정

--- project/blog/index.php
+++ project/blog/index.php
@@ -26,24 +26,24 @@ require_once "header.html";
             <a href="">
                 <div class="article">
                     <div class="title">자바의 특징?</div>
-                    <div class="info">자바는 객체 지향 프로그래밍 언어로써 웹 개발에 가장 널리 사용되는 언어 중 하나이다.</div>
+                    <div class="content">자바는 객체 지향 프로그래밍 언어로써 웹 개발에 가장 널리 사용되는 언어 중 하나이다.</div>
                 </div>
             </a>
 
             <a href="">
                 <div class="article">
                     <div class="title">자바의 특징?</div>
-                    <div class="info">자바는 객체 지향 프로그래밍 언어로써 웹 개발에 가장 널리 사용되는 언어 중 하나이다.</div>
-                </div>
-            </a>
-
-            <a href="">
-                <div class="article">
-                    <div class="title">자바의 특징?</div>
-                    <div class="info">자바는 객체 지향 프로그래밍 언어로써 웹 개발에 가장 널리 사용되는 언어 중 하나이다.</div>
+                    <div class="content">자바는 객체 지향 프로그래밍 언어로써 웹 개발에 가장 널리 사용되는 언어 중 하나이다.</div>
                 </div>
             </a>
             
+            <a href="">
+                <div class="article">
+                    <div class="title">자바의 특징?</div>
+                    <div class="content">자바는 객체 지향 프로그래밍 언어로써 웹 개발에 가장 널리 사용되는 언어 중 하나이다.</div>
+                </div>
+            </a>
+
         </div>
         <div class="board">
             <h1>BOARD</h1>
@@ -51,21 +51,21 @@ require_once "header.html";
             <a href="">
                 <div class="article">
                     <div class="title">자바의 특징?</div>
-                    <div class="info">자바는 객체 지향 프로그래밍 언어로써 웹 개발에 가장 널리 사용되는 언어 중 하나이다.</div>
+                    <div class="content">자바는 객체 지향 프로그래밍 언어로써 웹 개발에 가장 널리 사용되는 언어 중 하나이다.</div>
                 </div>
             </a>
 
             <a href="">
                 <div class="article">
                     <div class="title">자바의 특징?</div>
-                    <div class="info">자바는 객체 지향 프로그래밍 언어로써 웹 개발에 가장 널리 사용되는 언어 중 하나이다.</div>
+                    <div class="content">자바는 객체 지향 프로그래밍 언어로써 웹 개발에 가장 널리 사용되는 언어 중 하나이다.</div>
                 </div>
             </a>
 
             <a href="">
                 <div class="article">
                     <div class="title">자바의 특징?</div>
-                    <div class="info">자바는 객체 지향 프로그래밍 언어로써 웹 개발에 가장 널리 사용되는 언어 중 하나이다.</div>
+                    <div class="content">자바는 객체 지향 프로그래밍 언어로써 웹 개발에 가장 널리 사용되는 언어 중 하나이다.</div>
                 </div>
             </a>
 
